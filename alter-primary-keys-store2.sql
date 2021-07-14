43d0680426d66294f6adb543ada4663a9388de9b
Evan-D-Clark <eclark565678@gmail.com>
2021-07-14 13:41:03 -0700

    Create alter-primary-keys-store2.sql

---
+++ alter-primary-keys-store2.sql
@@ -0,0 +1,8 @@
+ALTER TABLE orders
+    ADD PRIMARY KEY (order_id),
+    DROP PRIMARY KEY,
+    DROP FOREIGN KEY fk_orders_customers,
+    ADD FOREIGN KEY fk_orders_customers (customer_id)
+        REFERENCES customers (customer_id)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION;
